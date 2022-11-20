a4e6c545679060258ff89538407a460af95ceec8
ABrogliatti <agustinbrogliatti@tharsa.com.ar>
2022-11-20 13:06:48 -0300

    fix eliminarPedido()

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #define ID_HAMBURGUESA 102
 
 #define LOG_DIR "log.txt"
-#define PEDIDOS_DIR "pedidos.txt"
+#define PEDIDOS_DIR "pedidosEntregados.txt"
 
 typedef struct {
     int id;
@@ -30,22 +30,24 @@ typedef struct Elemento {
 } Nodo;
 
 //TODO RENAME HEAD->CABEZA
-Pedido * configurarCatalogo(Pedido * pedidoSemilla);
-int resetearLog();
 void ingresarPedido(Nodo **head, Pedido * pedidoSemilla, int *ptrCantPedidos, int *ptrPreparandose, int *cantSalchichas, int *cantHamburguesas);
 void finalizarPedido(Nodo **head, int *ptrPreparandose);
 void emitirReporte(Nodo *head, int *ptrPreparandose);
-void guardarDatosVentas(int *cantSalchichas, int *cantHamburguesas);
-void salir();
+int salir();
 
+//HELPERS
+int resetearLog();
+Pedido * configurarCatalogo(Pedido * pedidoSemilla);
 Nodo * crearNodo(Pedido pedido);
 int insertarFinal(Nodo **head, Pedido datos);
-int eliminarPedido(Nodo ** head, int id);
-
+void guardarDatosVentas(int *cantSalchichas, int *cantHamburguesas);
+int mostrarPedidos(Nodo * head);
+Nodo * buscarPedido(Nodo * head, int id) ;
+void eliminarPedido(Nodo ** head, int id);
 
 int main() {
     Nodo *head = NULL;
-    int opcion=0, cantPedidos=0, preparandose=0, cantSalchichas=0, cantHamburguesas=0;
+    int opcion=0, cantPedidos=0, preparandose=0, cantSalchichas=0, cantHamburguesas=0, flag=0;
 
     int *ptrCantPedidos = &cantPedidos, *ptrPreparandose = &preparandose;
     int *ptrCantSalchichas = &cantSalchichas, *ptrCantHamburguesas = &cantHamburguesas;
@@ -89,7 +91,7 @@ int main() {
                 emitirReporte(head, ptrPreparandose);
                 break;
             case 4:
-                salir();
+                flag = salir();
                 break;
             default:
                 printf("Opcion no reconocida, vuelva a intentarlo...\n");
@@ -97,51 +99,11 @@ int main() {
         }
         printf("\n");
         system("pause");
-    } while (opcion != 4);
+    } while (flag != 1);
 
     return EXIT_SUCCESS;
 }
 
-int resetearLog() {
-    FILE * archivo = fopen(LOG_DIR, "w");
-    if (archivo == NULL) {
-        printf("ERROR: No se pudo abrir el archivo log.txt...");
-        system("pause");
-        return EXIT_FAILURE;
-    }
-
-    fclose(archivo);
-    return 0;
-}
-
-Pedido * configurarCatalogo(Pedido * pedidoSemilla) {
-    
-    Pedido *aux = NULL;
-    aux = (Pedido *) malloc(sizeof(Pedido));
-
-    if (aux == NULL) {
-        printf("MEMORY ERROR: Error al asignar espacio en memoria. funcion:['configurarPrograma']\n");
-        printf("La semilla del catalogo no se ha podido crear.\n");
-        system("pause");
-        return NULL;
-    }
-
-    pedidoSemilla = aux;
-
-    // Definicion del catalogo de productos
-    pedidoSemilla->salchicha.id = ID_SALCHICHA;
-    strcpy(pedidoSemilla->salchicha.nombre, "Salchicha");
-    pedidoSemilla->salchicha.precio = PRECIO_SALCHICHA;
-    pedidoSemilla->salchicha.cantidad = 0;
-
-    pedidoSemilla->hamburguesa.id = ID_HAMBURGUESA;
-    strcpy(pedidoSemilla->hamburguesa.nombre, "Hamburguesa XXL");
-    pedidoSemilla->hamburguesa.precio = PRECIO_HAMBURGUESA;
-    pedidoSemilla->hamburguesa.cantidad = 0;
-
-    return pedidoSemilla;
-
-}
 
 void ingresarPedido(Nodo **head, Pedido * nuevoPedido, int *ptrCantPedidos, int *ptrPreparandose, int *cantSalchichas, int *cantHamburguesas) {
     char opcion[2];
@@ -210,7 +172,46 @@ void ingresarPedido(Nodo **head, Pedido * nuevoPedido, int *ptrCantPedidos, int 
     return;
 }
 
-void finalizarPedido(Nodo **head, int *ptrPreparandose) {}
+void finalizarPedido(Nodo **head, int *ptrPreparandose) {
+    int flag=0, busquedaId=0;
+    Nodo * pedido = NULL;
+    FILE * archivo = NULL;
+
+    if(mostrarPedidos(*head) == 1) {
+        printf("No hay pedidos realizandose en este momento.\n"); //Valido que la lista no este vacia
+        return;
+    }
+
+    printf("\nIngrese el numero del pedido que desea finalizar: ");
+    scanf("%d", &busquedaId);
+
+    pedido = buscarPedido(*head, busquedaId);
+    if(pedido == NULL) {
+        printf("No se encontro el pedido especificado");
+        system("pause");
+        return;
+    }
+    
+    //Si se encontro un pedido con ese ID, guardo los datos de la venta antes de eliminarlos de la lista
+    archivo = fopen(PEDIDOS_DIR, "a");
+    if(archivo == NULL) {
+        printf("\nEl archivo pedidosEntregados.txt no se pudo abrir.");
+        system("pause");
+        return;
+    }
+
+    fprintf(archivo, "---Pedido %d---\n", pedido->pedido.id); //guardo el pedido a eliminar en el archivo pedidosEntregados.txt
+    fprintf(archivo, "\nSalchichas: %d a $%.2f c/u", pedido->pedido.salchicha.cantidad, pedido->pedido.salchicha.precio);
+    fprintf(archivo, "\nHamburguesas: %d a $%.2f c/u\n\n", pedido->pedido.hamburguesa.cantidad, pedido->pedido.hamburguesa.precio);
+    fclose(archivo);
+
+    //Luego de guardar el pedido en el registro, lo elimino de la lista.
+    eliminarPedido(head, busquedaId);
+    *ptrPreparandose = *ptrPreparandose - 1;
+    system("cls");
+    printf("Pedido finalizado con exito!");
+    return;
+}
 
 void emitirReporte(Nodo *head, int *ptrPreparandose) { // Incompleta
     FILE * archivo = fopen(LOG_DIR, "r");
@@ -232,25 +233,65 @@ void emitirReporte(Nodo *head, int *ptrPreparandose) { // Incompleta
     fclose(archivo);
 }
 
-void guardarDatosVentas(int *cantSalchichas, int *cantHamburguesas) {
+
+int salir() {
+    char respuesta[2];
+
+    printf("Esta seguro que desea salir? S/N");
+    fflush(stdin);
+    gets(respuesta);
+
+    if(strcasecmp(respuesta, "s") == 0) {
+        system("cls");
+        printf("\n-------------------------------\nGracias por utilizar Gordsys...\n-------------------------------\n\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+
+//HELPERS - HELPERS -HELPERS - HELPERS
+
+int resetearLog() {
     FILE * archivo = fopen(LOG_DIR, "w");
-    
     if (archivo == NULL) {
         printf("ERROR: No se pudo abrir el archivo log.txt...");
         system("pause");
-        return;
+        return EXIT_FAILURE;
     }
 
-    fprintf(archivo, "Cantidad de Salchichas vendidas: %d\n", *cantSalchichas);
-    fprintf(archivo, "Cantidad de Hamburguesas vendidas: %d\n", *cantHamburguesas);
-
-    fprintf(archivo, "SubTotal facturado en Salchichas: %.2f\n", (float)(*cantSalchichas * PRECIO_SALCHICHA));
-    fprintf(archivo, "SubTotal facturado en Hamburguesas: %.2f\n", (float)(*cantHamburguesas * PRECIO_HAMBURGUESA));
-
-    // TODO CAMBIO DE IMPRESIÓN
-
     fclose(archivo);
-    return;
+    return 0;
+}
+
+Pedido * configurarCatalogo(Pedido * pedidoSemilla) {
+    
+    Pedido *aux = NULL;
+    aux = (Pedido *) malloc(sizeof(Pedido));
+
+    if (aux == NULL) {
+        printf("MEMORY ERROR: Error al asignar espacio en memoria. funcion:['configurarPrograma']\n");
+        printf("La semilla del catalogo no se ha podido crear.\n");
+        system("pause");
+        return NULL;
+    }
+
+    pedidoSemilla = aux;
+
+    // Definicion del catalogo de productos
+    pedidoSemilla->salchicha.id = ID_SALCHICHA;
+    strcpy(pedidoSemilla->salchicha.nombre, "Salchicha");
+    pedidoSemilla->salchicha.precio = PRECIO_SALCHICHA;
+    pedidoSemilla->salchicha.cantidad = 0;
+
+    pedidoSemilla->hamburguesa.id = ID_HAMBURGUESA;
+    strcpy(pedidoSemilla->hamburguesa.nombre, "Hamburguesa XXL");
+    pedidoSemilla->hamburguesa.precio = PRECIO_HAMBURGUESA;
+    pedidoSemilla->hamburguesa.cantidad = 0;
+
+    return pedidoSemilla;
+
 }
 
 Nodo * crearNodo(Pedido pedido) {
@@ -301,35 +342,89 @@ int insertarFinal(Nodo **head, Pedido nuevoPedido) {
     return 0;
 }
 
-int eliminarPedido(Nodo ** head, int id) {
+void guardarDatosVentas(int *cantSalchichas, int *cantHamburguesas) {
+    FILE * archivo = fopen(LOG_DIR, "w");
+    
+    if (archivo == NULL) {
+        printf("ERROR: No se pudo abrir el archivo log.txt...");
+        system("pause");
+        return;
+    }
+
+    fprintf(archivo, "Cantidad de Salchichas vendidas: %d\n", *cantSalchichas);
+    fprintf(archivo, "Cantidad de Hamburguesas vendidas: %d\n", *cantHamburguesas);
+
+    fprintf(archivo, "SubTotal facturado en Salchichas: %.2f\n", (float)(*cantSalchichas * PRECIO_SALCHICHA));
+    fprintf(archivo, "SubTotal facturado en Hamburguesas: %.2f\n", (float)(*cantHamburguesas * PRECIO_HAMBURGUESA));
+
+    // TODO CAMBIO DE IMPRESIÓN
+
+    fclose(archivo);
+    return;
+}
+
+int mostrarPedidos(Nodo * head) {
     Nodo * actual=NULL;
-    int flag=0;
+    actual = head;
+
+    if(actual == NULL) {
+        return 1;
+    }
+
+    system("cls");
+    printf("------------------\nPedidos en proceso\n------------------");
+
+    while(actual != NULL) {
+        printf("\n\n------Pedido %d------", actual->pedido.id);
+        printf("\nCliente: %s", actual->pedido.cliente);
+
+        if (actual->pedido.salchicha.cantidad != 0) {
+           printf("\nSalchichas: %d", actual->pedido.salchicha.cantidad);
+        }
+
+        if (actual->pedido.hamburguesa.cantidad != 0) {
+           printf("\nHamburguesas: %d", actual->pedido.hamburguesa.cantidad);
+        }
+        
+        actual = actual->siguiente;
+    }
+
+    printf("\n");
+    system("pause");
+    return 0;
+}
+
+Nodo * buscarPedido(Nodo * head, int id) {
+    Nodo * busqueda = NULL, * actual = NULL;
+    actual = head;
+
+    while (actual != NULL){
+        if (actual->pedido.id == id) {
+            return actual;
+        }
+        actual = actual->siguiente;
+    }
+
+    return NULL;
+}
+
+void eliminarPedido(Nodo ** head, int id) {
+    Nodo * actual=NULL;
 
     actual = *head;
-    if (actual == NULL) {
-        printf("No se puede eliminar el pedido porque lista esta vacias. funcion:['eliminarNodo']\n");
-        return -1;
-    } 
-
     if (actual->pedido.id == id) { //Si el que deseo eliminar es cabeza, reasigno cabeza al siguiente.
         *head = actual->siguiente;
         free(actual);
-
     } else {
         for(;actual->siguiente;) {
             if (actual->siguiente->pedido.id == id) { //ya parto buscando desde la 2da pos, la 1ra ya fue revisada.
                 actual->siguiente = actual->siguiente->siguiente; //Si el nodo sucesor tiene el id que busco, hago que el nodo actual apunte al siguiente del siguiente, osea lo saltea, elimininandolo del enlace.
                 free(actual->siguiente);
-                flag=1;
                 break;
             }
             actual = actual->siguiente;
         }
     }
 
-    return flag;
+    return;
 }
-
-
-
-void salir() {}
